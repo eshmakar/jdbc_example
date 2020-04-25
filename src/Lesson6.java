b13e7028876c41deadb89f5027bebccddddb019f
Marat <eshmakar@mail.ru>
2020-04-25 14:27:54 +0300

    Код автора видеокурса (Lesson6)

--- src/Lesson6.java
+++ src/Lesson6.java
@@ -1,58 +1,86 @@
 import java.sql.*;
 
 public class Lesson6 {
+    // Задаем параметры подключения, но делаем их статическими
     static String url = "jdbc:mysql://localhost:3306/first_lesson?useSSL=false&serverTimezone=UTC";
     static String userName = "root";
     static String password = "root";
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException, InterruptedException {
-//типы чтения и уровни изоляции в транзакциях
-        try (Connection connection = DriverManager.getConnection(url, userName, password);
-             Statement stat = connection.createStatement()) {
-            connection.setAutoCommit(false); //отключаем автофиксацию
-//            connection.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED); //грязное чтение
-//
-//            //грязное чтение (при грязном чтении транзакция может работать с данными, которые нет в таблицах)
-//            stat.executeUpdate("update books set price = 100 where bookId =1"); //обновляем данные в первой строки, не будем сразу вносить изменения в базу, а создадим вторую транзакцию, которая будет работать в другом потоке
-//            new otherTransaction().start(); //вызываем второй поток, который будет выводит все кники
+    public static void main(String[] args) throws SQLException, InterruptedException {
+        // Создаем подключение и получаем объект типа Statement
+        try(Connection conn = DriverManager.getConnection(url, userName, password);
+            Statement stat = conn.createStatement()) {
+            // Отключаем режим автоматической фиксации результатов выполнения команд SQL
+            conn.setAutoCommit(false);
+
+            // dirty read
+//            conn.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED); // чтение разрешено
+//            stat.executeUpdate("UPDATE Books SET price = 100 WHERE bookId = 1");
+//            new OtherTransaction().start();
 //            Thread.sleep(2000);
-//            connection.rollback();//делаем откат
+//            conn.rollback();
 
-//            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);//Установливаем уровень изоляции транзакции, на который разрешается неповторяющиеся чтения
-            connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
-            ResultSet resultSet = stat.executeQuery("Select * from books");
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("name") + " " + " " + resultSet.getDouble(3));
+            // non-repeatable read
+//            conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // чтение разрешено
+            conn.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ); // чтение заблокировано (это значение достаточно установить в одной транзакции)
+//            ResultSet resultSet = stat.executeQuery("SELECT * FROM Books");
+//            while (resultSet.next()) {
+//                System.out.println(resultSet.getString("name") + " " + resultSet.getDouble(3));
+//            }
+//            new OtherTransaction().start();
+//            Thread.sleep(2000);
+//
+//            ResultSet resultSet2 = stat.executeQuery("SELECT * FROM Books");
+//            while (resultSet2.next()) {
+//                System.out.println(resultSet2.getString("name") + " " + resultSet2.getDouble(3));
+//            }
+
+            // phantom read
+//            conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // чтение разрешено
+            conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE); // чтение заблокировано
+            ResultSet resultSet = stat.executeQuery("SELECT * FROM Books WHERE bookId > 5");
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name") + " " + resultSet.getDouble(3));
             }
+            new OtherTransaction().start();
             Thread.sleep(2000);
-            connection.rollback();
 
-            ResultSet resultSet2 = stat.executeQuery("Select * from books");
-            while (resultSet2.next()){
-                System.out.println(resultSet2.getString("name") + " " + " " + resultSet2.getDouble(3));
+            ResultSet resultSet2 = stat.executeQuery("SELECT * FROM Books WHERE bookId > 5");
+            while (resultSet2.next()) {
+                System.out.println(resultSet2.getString("name") + " " + resultSet2.getDouble(3));
             }
         }
     }
 
-    static class otherTransaction extends Thread {
+    // Класс, в котором реализуются транзакции для чтения
+    static class OtherTransaction extends Thread {
         @Override
         public void run() {
-            try (Connection connection = DriverManager.getConnection(url, userName, password);
-                 Statement stat = connection.createStatement()) {
-                connection.setAutoCommit(false);
-//                connection.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED); //грязное чтение
-//                ResultSet rs = stat.executeQuery("SELECT * from books"); //второй поток при этом получает все книги
-//                while (rs.next()){//потом выводим имя и цену
-//                    System.out.println(rs.getString("name") + " " + " price " + rs.getDouble(3));
-//                }connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-//                connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);//Установливаем уровень изоляции транзакции, на который разрешается неповторяющиеся чтения
-                connection.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ);
-                stat.executeUpdate("UPDATE books set price = price+20 where bookId = 6"); //обновляем цену на книгу
-                connection.commit();//вносим изменения в БД!
+            try (Connection conn = DriverManager.getConnection(url, userName, password);
+                 Statement stat = conn.createStatement()) {
+                // Отключаем режим автоматической фиксации результатов выполнения команд SQL
+                conn.setAutoCommit(false);
 
+                // dirty read
+//                conn.setTransactionIsolation(Connection.TRANSACTION_READ_UNCOMMITTED); // чтение заблокировано
+//                ResultSet rs = stat.executeQuery("SELECT * FROM Books");
+//                while (rs.next()) {
+//                    System.out.println(rs.getString("name") + " " + rs.getDouble(3));
+//                }
 
-            } catch (SQLException e) {
-                e.printStackTrace();
+                // non-repeatable read
+//                conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // чтение разрешено
+//                conn.setTransactionIsolation(Connection.TRANSACTION_REPEATABLE_READ); // чтение заблокировано
+//                stat.executeUpdate("UPDATE Books SET price = price + 20 WHERE name = 'Solomon key'");
+//                conn.commit();
+
+                // phantom read
+//                conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED); // чтение разрешено
+                conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE); // чтение заблокировано
+                stat.executeUpdate("INSERT INTO Books (name, price) VALUES ('new Book', 10)");
+                conn.commit();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
             }
         }
     }
